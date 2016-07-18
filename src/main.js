eebfb5a45ca849a9018f2a5a0c110a57208e6b2d
JD Cantrell <jd@goodrobot.net>
2016-07-18 01:16:18 -0700

    Destroy the world, start from scratch with nothing

--- src/main.js
+++ src/main.js
@@ -1,107 +1,62 @@
-import Rx from 'rx';
-import Cycle from '@cycle/core';
-import { ul, li, makeDOMDriver } from '@cycle/dom';
+const $ = (selector) => Array.prototype.slice.call(document.querySelectorAll(selector));
 
-const initialState = {
-  ui: {
-    selected: null,
-  },
-  items: [
-    { id: 'one', text: 'one' },
-    { id: 'two', text: 'two' },
-    { id: 'three', text: 'three' },
-    { id: 'four', text: 'four' },
-  ]
-};
+const on = (nodes, event, fn) => nodes.forEach((node) => node.addEventListener(event, fn));
 
-const keymap = {
-  move: {
-    ArrowUp: -1,
-    ArrowDown: 1,
-  }
-};
+const draggables = $('li[draggable]');
 
-function intents({ DOM, Keypress }) {
-  return {
-    keypress$: Keypress.map(ev => {
-      console.log(ev.key, ev);
-      return ev.key;
-    }),
-    select$: DOM.select('li').events('click').map(ev => ev.target.id),
-    swapItems$: Rx.Observable.combineLatest(
-      DOM.select('li').events('dragstart').map((ev) => {
-        ev.dataTransfer.setData('text', ev.target.id);
-        return ev.target.id;
-      }),
-      DOM.select('li').events('dragover').map((ev) => ev.target.id)
-    )
-  };
-}
+let id = 0;
+draggables.forEach(el => { el.id = id; id += 1; });
 
-function model({ select$, swapItems$, keypress$ }) {
-  const swap$ = swapItems$.map((swapIds) => state => {
-    const src = swapIds[0];
-    const dest = swapIds[1];
-    if (src !== dest) {
-      const next = Object.assign({}, state);
-      next.items = state.items.map((item) => {
-        if (item.id === src) {
-          return state.items.find((i) => i.id === dest);
-        }
-        if (item.id === dest) {
-          return state.items.find((i) => i.id === src);
-        }
-        return item;
-      });
-      return next;
+let dragEl = null;
+
+const dom = {
+  next: (el) => {
+    const n = el.nextSibling;
+    if (n && n.nodeType !== 1) {
+      return dom.next(n);
     }
-    return state;
-  });
+    return n;
+  },
+  previous: (el) => {
+    const n = el.previousSibling;
+    if (n && n.nodeType !== 1) {
+      return dom.previous(n);
+    }
+    return n;
+  },
+};
 
-  const $commandMode = keypress$.filter(key => key === 'Escape').map(() => state => Object.assign({}, state, { ui: { selected: null } }));
-  const $moveItem = keypress$.map(key => keymap.move[key]).filter(moveDir => typeof moveDir !== 'undefined').map(moveDir => state => {
-    if (state.ui.selected) {
-      const idx = state.items.findIndex(item => item.id === state.ui.selected);
-      const swapIdx = idx + moveDir;
-      const next = Object.assign({}, state);
-      if (typeof next.items[swapIdx] !== 'undefined') {
-        const t = next.items[idx];
-        next.items[idx] = next.items[swapIdx];
-        next.items[swapIdx] = t;
-        console.log(next.items);
-        return next;
+
+on(draggables, 'dragstart', (ev) => {
+  ev.dataTransfer.setData('text/plain', ev.target.id);
+  dragEl = ev.target;
+});
+
+let lastMoveEl = null;
+on(draggables, 'dragover', (ev) => {
+  if (ev.currentTarget !== dragEl && ev.currentTarget !== lastMoveEl) {
+    const prevEl = dom.previous(ev.currentTarget);
+    if (prevEl !== dragEl) {
+      ev.currentTarget.parentNode.insertBefore(dragEl, ev.currentTarget);
+    } else {
+      const nextEl = dom.next(ev.currentTarget);
+      if (nextEl) {
+        ev.currentTarget.parentNode.insertBefore(dragEl, nextEl);
+      } else {
+        ev.currentTarget.parentNode.appendChild(dragEl);
       }
     }
-    return state;
-  });
+    lastMoveEl = ev.currentTarget;
+  }
+  return 1;
+});
 
-  const $s = select$.map(id => state => Object.assign({}, state, { ui: { selected: id } }));
-  return Rx.Observable.merge(swap$, $s, $commandMode, $moveItem)
-    .scan((state, operation) => operation(state), initialState);
-}
 
-function view(state$) {
-  return state$.startWith(initialState).map(
-    ({ items, ui }) => ul(
-      items.map(item => {
-        if (ui.selected === item.id) {
-          return li('.selected', { id: item.id, draggable: true }, item.text);
-        }
-        return li({ id: item.id, draggable: true }, item.text);
-      })
-    )
-  );
-}
+on($('[contenteditable]'), 'click', (ev) => {
+  const ce = ev.currentTarget.getAttribute('contentEditable');
+  if (ce !== 'true') {
+    ev.currentTarget.setAttribute('contentEditable', true);
+  }
+  return true;
+});
 
-function main({ DOM, Keypress }) {
-  return {
-    DOM: view(model(intents({ DOM, Keypress })))
-  };
-}
-
-const drivers = {
-  DOM: makeDOMDriver('#kodiak'),
-  Keypress: () => Rx.Observable.fromEvent(document, 'keypress')
-};
-
-Cycle.run(main, drivers);
